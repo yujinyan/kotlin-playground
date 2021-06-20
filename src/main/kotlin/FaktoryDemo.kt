24c2486cd1a45fc71991bd1fb0c2822b8643fe28
Yu Jinyan <yujinyan@outlook.com>
2021-06-20 15:10:42 +0800

    delete files

--- src/main/kotlin/FaktoryDemo.kt
+++
@@ -1,8 +0,0 @@
-import com.jojotoo.api.user.HelpService
-import me.yujinyan.facktory.factory
-
-val p = factory<Int>()
-
-fun main() {
-  val a: HelpService
-}
--- src/main/kotlin/Loop.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-  for (i in f()) {
-    println(i)
-  }
-}
-
-fun f() = listOf(1, 2, 3)
